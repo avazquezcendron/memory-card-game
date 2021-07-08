f9f1f8ba6e49ed3af5e6dcd2691aab975b7478ab
ElAngelGris05 <agustiki@hotmail.com>
2021-07-07 21:24:42 -0300

    Firs attempt

---
+++ js/scripts.js
@@ -0,0 +1,127 @@
+'use strict';
+
+var cardsArray = [{
+  'name': 'chivi',
+  'img': 'img/chivi.png'
+}, {
+  'name': 'elo',
+  'img': 'img/elo.png'
+}, {
+  'name': 'fredy',
+  'img': 'img/fredy.png'
+}, {
+  'name': 'carmen',
+  'img': 'img/carmen.png'
+}, {
+  'name': 'tay',
+  'img': 'img/tay.png'
+}, {
+  'name': 'tay2',
+  'img': 'img/tay2.png'
+}, {
+  'name': 'marcos',
+  'img': 'img/marcos.png'
+}, {
+  'name': 'montse',
+  'img': 'img/montse.png'
+}, {
+  'name': 'runa',
+  'img': 'img/runa.png'
+}, {
+  'name': 'negrita',
+  'img': 'img/negrita.png'
+}, {
+  'name': 'agus',
+  'img': 'img/agus.png'
+}, {
+  'name': 'chino',
+  'img': 'img/chino.png'
+}];
+
+var gameGrid = cardsArray.concat(cardsArray).sort(function () {
+  return 0.5 - Math.random();
+});
+
+var firstGuess = '';
+var secondGuess = '';
+var count = 0;
+var previousTarget = null;
+var delay = 1200;
+
+var game = document.getElementById('game');
+var grid = document.createElement('section');
+grid.setAttribute('class', 'grid');
+game.appendChild(grid);
+
+
+var createGrid = () => gameGrid.forEach(function (item) {
+  var name = item.name,
+      img = item.img;
+
+
+  var card = document.createElement('div');
+  card.classList.add('card');
+  card.dataset.name = name;
+
+  var front = document.createElement('div');
+  front.classList.add('front');
+
+  var back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = 'url(' + img + ')';
+
+  grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
+});
+
+var match = function match() {
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(function (card) {
+    card.classList.add('match');
+  });
+};
+
+var resetGuesses = function resetGuesses() {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+  previousTarget = null;
+
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(function (card) {
+    card.classList.remove('selected');
+  });
+};
+
+grid.addEventListener('click', function (event) {
+
+  var clicked = event.target;
+
+  if (clicked.nodeName === 'SECTION' || clicked === previousTarget || clicked.parentNode.classList.contains('selected') || clicked.parentNode.classList.contains('match')) {
+    return;
+  }
+
+  if (count < 2) {
+    count++;
+    if (count === 1) {
+      firstGuess = clicked.parentNode.dataset.name;
+      console.log(firstGuess);
+      clicked.parentNode.classList.add('selected');
+    } else {
+      secondGuess = clicked.parentNode.dataset.name;
+      console.log(secondGuess);
+      clicked.parentNode.classList.add('selected');
+    }
+
+    if (firstGuess && secondGuess) {
+      if (firstGuess === secondGuess) {
+        setTimeout(match, delay);
+      }
+      setTimeout(resetGuesses, delay);
+    }
+    previousTarget = clicked;
+  }
+});
+
+createGrid()
