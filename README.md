7f5836a194cdcb9c62df2818747fe90191910ba8
ElAngelGris05 <agustiki@hotmail.com>
2021-07-25 13:09:36 -0300

    Updating js and organizing the files

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
 
 
-VanillaJs Memory Card Game. Simple but fun 😁
+JQuery Memory Card Game. Simple but fun 😁
+
+> It has some customization for my little niece. ☺
 
 ---
 
@@ -11,24 +13,24 @@ Card memory is a game where you have to click on a card to see what image is und
 
 ## User Stories
 
-- User can see a grid with n x n cards (n is an integer). All the cards are faced down initially (hidden state)
+- [x]  User can see a grid with n x n cards (n is an integer). All the cards are faced down initially (hidden state)
   
-- User can click a button to start the game. When this button is clicked, a timer will start
+- [x]  User can click a button to start the game. When this button is clicked, a timer will start
   
-- User can click on any card to unveil the image that is underneath it (change it to visible state). The image will be displayed until the user clicks on a 2nd card
+- [x]  User can click on any card to unveil the image that is underneath it (change it to visible state). The image will be displayed until the user clicks on a 2nd card
   
-## When the User clicks on the 2nd card:
+### When the User clicks on the 2nd card:
 
-- If there is a match, the 2 cards will be eliminated from the game (either hide/remove them or leave them in the visible state)
+- [x]  If there is a match, the 2 cards will be eliminated from the game (either hide/remove them or leave them in the visible state)
   
-- If there isn’t a match, the 2 cards will flip back to their original state (hidden state)
+- [x]  If there isn’t a match, the 2 cards will flip back to their original state (hidden state)
   
-- When all the matches have been found, the User can see a dialog box showing a Congratulations message with a counter displaying the time it took to finish the game
+- [x]  When all the matches have been found, the User can see a dialog box showing a Congratulations message with a counter displaying the time it took to finish the game
   
 ## Bonus features
-- Use can choose between multiple levels of difficulty (Easy, Medium, Hard). Increased difficulty means: decreasing the time available to complete and/or increasing the number of cards
+- [ ]  User can choose between multiple levels of difficulty (Easy, Medium, Hard). Increased difficulty means: decreasing the time available to complete and/or increasing the number of cards
   
-- User can see the game statistics (nr. of times he won / he lost, best time for each level)
+- [ ] User can see the game statistics (nr. of times he won / he lost, best time for each level)
 
 ## Authors
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,180 +1,339 @@
-'use strict';
+var $board = $('main'),
+  $itemCount = $('.score span'),
+  $wins = $('.wins span'),
+  $turns = $('.turns span'),
+  $attempts = $('.attempts span'),
+  $attemptsOverall = $('.attempts-overall span'),
+  $success = $('.success'),
+  $successMsg = $('.success-message'),
+  $successIcon = $('.success-icon'),
+  $btnContinue = $('.btn-continue'),
+  $btnSound = $('.btn-sound'),
+  selectedClass = 'is-selected',
+  visibleClass = 'is-visible',
+  playSoundClass = 'is-playing',
+  scoreUpdateClass = 'is-updating',
+  lastTurnClass = 'last-turn',
+  dataMatch = 'data-matched',
+  dataType = 'data-type',
+  dataName = 'data-name',
+  turnsCount = 2,
+  winsCount = 0,
+  attemptsCount = 0,
+  attemptsOverallCount = 0,
+  tooManyAttempts = 8,
+  timeoutLength = 600,
+  card1,
+  card2,
+  msg;
 
-var cardsArrayFamily = [{
-  'name': 'chivi',
-  'img': 'img/chivi.png'
-}, {
-  'name': 'elo',
-  'img': 'img/elo.png'
-}, {
-  'name': 'fredy',
-  'img': 'img/fredy.png'
-}, {
-  'name': 'carmen',
-  'img': 'img/carmen.png'
-}, {
-  'name': 'tay',
-  'img': 'img/tay.png'
-}, {
-  'name': 'tay2',
-  'img': 'img/tay2.png'
-}, {
-  'name': 'marcos',
-  'img': 'img/marcos.png'
-}, {
-  'name': 'montse',
-  'img': 'img/montse.png'
-}, {
-  'name': 'runa',
-  'img': 'img/runa.png'
-}, {
-  'name': 'negrita',
-  'img': 'img/negrita.png'
-}, {
-  'name': 'agus',
-  'img': 'img/agus.png'
-}, {
-  'name': 'chino',
-  'img': 'img/chino.png'
-}];
+('use strict');
 
-var cardsArraySMB1 = [{
-  'name': 'shell',
-  'img': 'img/blueshell.png'
-}, {
-  'name': 'star',
-  'img': 'img/star.png'
-}, {
-  'name': 'bobomb',
-  'img': 'img/bobomb.png'
-}, {
-  'name': 'mario',
-  'img': 'img/mario.png'
-}, {
-  'name': 'luigi',
-  'img': 'img/luigi.png'
-}, {
-  'name': 'peach',
-  'img': 'img/peach.png'
-}, {
-  'name': '1up',
-  'img': 'img/1up.png'
-}, {
-  'name': 'mushroom',
-  'img': 'img/mushroom.png'
-}, {
-  'name': 'thwomp',
-  'img': 'img/thwomp.png'
-}, {
-  'name': 'bulletbill',
-  'img': 'img/bulletbill.png'
-}, {
-  'name': 'coin',
-  'img': 'img/coin.png'
-}, {
-  'name': 'goomba',
-  'img': 'img/goomba.png'
-}];
+var cardsArrayFamily = [
+  {
+    dataType: 'familyMember',
+    name: 'chivi',
+    img: '../img/chivi.png',
+  },
+  {
+    dataType: 'familyMember',
+    name: 'elo',
+    img: '../img/elo.png',
+  },
+  {
+    dataType: 'familyMember',
+    name: 'fredy',
+    img: '../img/fredy.png',
+  },
+  {
+    dataType: 'familyMember',
+    name: 'carmen',
+    img: '../img/carmen.png',
+  },
+  {
+    dataType: 'familyMember',
+    name: 'tay',
+    img: '../img/tay.png',
+  },
+  {
+    dataType: 'familyMember',
+    name: 'marcos',
+    img: '../img/marcos.png',
+  },
+  {
+    dataType: 'familyMember',
+    name: 'montse',
+    img: '../img/montse.png',
+  },
+  {
+    dataType: 'familyMember',
+    name: 'agus',
+    img: '../img/agus.png',
+  },
+  {
+    dataType: 'familyMember',
+    name: 'chino',
+    img: '../img/chino.png',
+  },
+  {
+    'dataType': 'familyMember',
+    'name': 'runa',
+    'img': '../img/runa.png'
+  }, {
+    'dataType': 'familyMember',
+    'name': 'negrita',
+    'img': '../img/negrita.png'
+  }, {
+    'dataType': 'familyMember',
+    'name': 'tay2',
+    'img': '../img/tay2.png'
+  }
+];
 
+var cardsArraySMB = [
+  {
+    dataType: 'flower',
+    name: 'flower',
+    img: 'img/blueshell.png',
+  },
+  {
+    dataType: 'mushroom',
+    name: 'mushroom',
+    img: 'img/star.png',
+  },
+  {
+    dataType: 'star',
+    name: 'star',
+    img: 'img/bobomb.png',
+  },
+  {
+    dataType: '1up',
+    name: '1up',
+    img: 'img/mario.png',
+  },
+  {
+    dataType: 'coin10',
+    name: 'coin10',
+    img: 'img/luigi.png',
+  },
+  {
+    dataType: 'coin20',
+    name: 'coin20',
+    img: 'img/peach.png',
+  },
+  {
+    dataType: 'flower',
+    name: 'flower',
+    img: 'img/blueshell.png',
+  },
+  {
+    dataType: 'mushroom',
+    name: 'mushroom',
+    img: 'img/star.png',
+  },
+  {
+    dataType: 'star',
+    name: 'star',
+    img: 'img/bobomb.png',
+  },
+];
+
+// Let's make some noise
+var assetsUrl = './sounds/',
+  sound = [
+    'smb3_1-up.mp3',
+    'smb3_nspade_match.mp3',
+    'smb3_bonus_game_no_match.mp3',
+  ],
+  $themeSongEl = $('#theme-song')[0],
+  soundSuccess = new Audio(assetsUrl + sound[0]),
+  soundMatch = new Audio(assetsUrl + sound[1]),
+  soundNoMatch = new Audio(assetsUrl + sound[2]);
+
+// Turn on the sound if you want that real deal throwback experience
+$btnSound.on('click', function (e) {
+  e.preventDefault();
+  $(this).toggleClass(playSoundClass);
+  if ($(this).hasClass(playSoundClass)) {
+    $themeSongEl.play();
+  } else {
+    $themeSongEl.pause();
+  }
+});
 
 const FAMILY = 'family';
 const SMB = 'smb';
 
-const MATCHTYPE = FAMILY | SMB;
-
 var matchType = SMB;
 
-var cardsArray = matchType == FAMILY ? cardsArrayFamily : cardsArraySMB1;
-// var cardsArray = cardsArrayFamily;
 
-var gameGrid = cardsArray.concat(cardsArray).sort(function () {
-  return 0.5 - Math.random();
-});
+//create the grid
+var createGrid = () => {
+  var cardsArray = matchType == FAMILY ? cardsArrayFamily : cardsArraySMB;
 
-var firstGuess = '';
-var secondGuess = '';
-var count = 0;
-var previousTarget = null;
-var delay = 1200;
+  cardsArray = cardsArray.sort(function () {
+    return 0.5 - Math.random();
+  }).slice(0, 9);
+  var gameGrid = cardsArray.concat(cardsArray);
 
-var game = document.getElementById('game');
-var grid = document.createElement('section');
-grid.setAttribute('class', 'grid');
-game.appendChild(grid);
+  $board.empty();
 
+  gameGrid.forEach(function (item) {
+    var card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.type = item.dataType;
+    card.dataset.matched = false;
+    card.dataset.name = item.name;
 
-var createGrid = () => gameGrid.forEach(function (item) {
-  var name = item.name,
-      img = item.img;
+    var front = document.createElement('div');
+    front.classList.add('front');
 
+    var back = document.createElement('div');
+    back.classList.add('back');
 
-  var card = document.createElement('div');
-  card.classList.add('card');
-  card.dataset.name = name;
-
-  var front = document.createElement('div');
-  front.classList.add('front');
-
-  var back = document.createElement('div');
-  back.style.backgroundImage = 'url(' + img + ')';
-  back.classList.add('back');
-  if (matchType == FAMILY)
-    back.classList.add('backFamily');
-  else    
-    back.classList.add('backSMB');  
-
-  grid.appendChild(card);
-  card.appendChild(front);
-  card.appendChild(back);
-});
-
-var match = function match() {
-  var selected = document.querySelectorAll('.selected');
-  selected.forEach(function (card) {
-    card.classList.add('match');
+    $board.append(card);
+    card.appendChild(front);
+    card.appendChild(back);
   });
-};
 
-var resetGuesses = function resetGuesses() {
-  firstGuess = '';
-  secondGuess = '';
-  count = 0;
-  previousTarget = null;
-
-  var selected = document.querySelectorAll('.selected');
-  selected.forEach(function (card) {
-    card.classList.remove('selected');
-  });
-};
-
-grid.addEventListener('click', function (event) {
-
-  var clicked = event.target;
-
-  if (clicked.nodeName === 'SECTION' || clicked === previousTarget || clicked.parentNode.classList.contains('selected') || clicked.parentNode.classList.contains('match')) {
-    return;
-  }
-
-  if (count < 2) {
-    count++;
-    if (count === 1) {
-      firstGuess = clicked.parentNode.dataset.name;
-      console.log(firstGuess);
-      clicked.parentNode.classList.add('selected');
-    } else {
-      secondGuess = clicked.parentNode.dataset.name;
-      console.log(secondGuess);
-      clicked.parentNode.classList.add('selected');
+  $card = $('.card');
+  $card.on('click', function () {
+    // Add selected class to a card only if it is not already matched
+    if ($(this).attr(dataMatch) == 'false') {
+      $(this).addClass(selectedClass);
     }
-
-    if (firstGuess && secondGuess) {
-      if (firstGuess === secondGuess) {
-        setTimeout(match, delay);
+  
+    var selectedCards = $('.' + selectedClass);
+  
+    // Check if cards match
+    if (selectedCards.length == 2) {
+      card1 = selectedCards.eq(0).attr(dataName);
+      card2 = selectedCards.eq(1).attr(dataName);
+  
+      if (card1 == card2) {
+        if ($btnSound.hasClass(playSoundClass)) {
+          soundMatch.play();
+        }
+        selectedCards.attr(dataMatch, true).removeClass(selectedClass);
+      } else {
+        if ($btnSound.hasClass(playSoundClass)) {
+          soundNoMatch.play();
+        }
+        setTimeout(function () {
+          turnsCount--;
+          $turns.addClass(scoreUpdateClass).html(turnsCount);
+          selectedCards.removeClass(selectedClass);
+        }, timeoutLength);
+  
+        if (turnsCount === 1) {
+          setTimeout(function () {
+            $turns.addClass(lastTurnClass);
+          }, timeoutLength);
+        }
+  
+        if (turnsCount <= 0) {
+          setTimeout(function () {
+            turnsCount = 2;
+            $turns.removeClass(lastTurnClass).html(turnsCount);
+            $card.attr(dataMatch, false);
+            attemptsCount += 1;
+            $attempts.addClass(scoreUpdateClass).html(attemptsCount);
+          }, timeoutLength);
+        }
       }
-      setTimeout(resetGuesses, delay);
     }
-    previousTarget = clicked;
+  
+    // Winner!
+    if ($('[' + dataMatch + '="true"]').length == $card.length) {
+      // Show success screen
+      $success.addClass(visibleClass);
+      if (attemptsCount <= tooManyAttempts) {
+        setTimeout(function () {
+          if ($btnSound.hasClass(playSoundClass)) {
+            soundSuccess.play();
+          }
+        }, 600);
+      }
+      // Update success message based on the amount of attempts
+      switch (true) {
+        case attemptsCount <= 2:
+          msg = 'SUPER!!!';
+          $successIcon.attr(dataType, 'star');
+          break;
+        case attemptsCount > 2 && attemptsCount <= 5:
+          msg = 'Bien Hecho!';
+          if (matchType == FAMILY) {
+            $successIcon.attr(dataType, 'familyMember');
+            $successIcon.attr(dataName, 'elo');
+          } else $successIcon.attr(dataType, 'mushroom');
+          break;
+        case attemptsCount > 5 && attemptsCount <= 8:
+          msg = 'Puedes hacerlo mejor!';
+          $successIcon.attr(dataType, 'flower');
+          break;
+        case attemptsCount > tooManyAttempts:
+          msg = 'Eso llevó tiempo...';
+          $successIcon.attr(dataType, 'chest');
+          break;
+      }
+      $successMsg.text(msg);
+  
+      setTimeout(function () {
+        attemptsOverallCount += attemptsCount;
+        $attemptsOverall.addClass(scoreUpdateClass).html(attemptsOverallCount);
+        winsCount += 1;
+        $wins.addClass(scoreUpdateClass).html(winsCount);
+        $card.attr(dataMatch, false);
+      }, 1200);
+    }
+  });
+
+}
+  
+  
+
+var startGame = () => {
+  $success.addClass(visibleClass);
+  $successIcon.attr(dataType, 'familyMember');
+  $successIcon.attr(dataName, 'elo');
+  $successMsg.text('Quieres jugar conmigo? 😀');
+  $btnContinue.text('Siiiii! Quiero Jugar');
+};
+
+startGame();
+
+
+
+// Remove the score update class after the animation completes
+$itemCount.on(
+  'webkitAnimationEnd oanimationend msAnimationEnd animationend',
+  function () {
+    $itemCount.removeClass(scoreUpdateClass);
   }
+);
+
+// On to the next round!
+$btnContinue.on('click', function () {
+  $success.removeClass(visibleClass);
+  createGrid();
+  shuffleCards();
+  setTimeout(function () {
+    turnsCount = 2;
+    $turns.removeClass(lastTurnClass).html(turnsCount);
+    attemptsCount = 0;
+    $attempts.html(attemptsCount);
+  }, 300);
 });
 
-createGrid()
+// Card shuffle function
+function shuffleCards() {
+  var cards = $board.children();
+  while (cards.length) {
+    $board.append(cards.splice(Math.floor(Math.random() * cards.length), 1)[0]);
+  }
+}
+
+function setMatchType(value) {
+  matchType = value;
+  $btnContinue.show();
+}
